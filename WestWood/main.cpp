c44cc52ff0cd2da2721c023cbc69c2585a638104
jacklinmaker <jacklinmaker@gmail.com>
2013-10-26 21:01:58 +0800

    Initial Commit

---
+++ WestWood/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WestWood
+//
+//  Created by 林光海 on 13-10-26.
+//  Copyright (c) 2013年 林光海. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
